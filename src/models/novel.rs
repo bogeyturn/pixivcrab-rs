04b39c76e3e4af52376bc2c0d0c8d5b24dbabfa0
bogeyturn <bogey-turn-0v@icloud.com>
2025-07-14 21:46:41 +0200

    make everything pub

--- src/models/novel.rs
+++ src/models/novel.rs
@@ -53,28 +53,28 @@ pub struct NovelTextResponse {
 #[derive(Deserialize, Serialize, Clone, Debug, PartialEq, Eq)]
 #[serde(rename_all = "camelCase")]
 pub struct WebViewNovelResponse {
-    title: String,
-    user_id: String,
-    text: String,
-    tags: Vec<String>,
-    series_title: Option<String>,
-    series_id: Option<String>,
-    rating: Rating,
-    is_original: bool,
-    images: ArrayOrMap<Image>,
-    id: String,
-    cover_url: String,
-    cdate: String,
-    caption: String,
-    ai_type: i64,
-    series_is_watched: Option<bool>,
-    series_navigation: Option<SeriesNavigation>,
-    marker: Option<Value>,
-    seasonal_effect_animation_urls: Option<Value>,
-    replaceable_item_ids: Option<Vec<Value>>,
-    glossary_items: Vec<Value>,
-    illusts: ArrayOrMap<Value>,
-    event_banners: Option<Value>,
+    pub title: String,
+    pub user_id: String,
+    pub text: String,
+    pub tags: Vec<String>,
+    pub series_title: Option<String>,
+    pub series_id: Option<String>,
+    pub rating: Rating,
+    pub is_original: bool,
+    pub images: ArrayOrMap<Image>,
+    pub id: String,
+    pub cover_url: String,
+    pub cdate: String,
+    pub caption: String,
+    pub ai_type: i64,
+    pub series_is_watched: Option<bool>,
+    pub series_navigation: Option<SeriesNavigation>,
+    pub marker: Option<Value>,
+    pub seasonal_effect_animation_urls: Option<Value>,
+    pub replaceable_item_ids: Option<Vec<Value>>,
+    pub glossary_items: Vec<Value>,
+    pub illusts: ArrayOrMap<Value>,
+    pub event_banners: Option<Value>,
 }
 
 #[derive(Deserialize, Serialize, Clone, Debug, PartialEq, Eq)]
@@ -87,55 +87,55 @@ pub enum ArrayOrMap<V> {
 #[derive(Deserialize, Serialize, Clone, Debug, PartialEq, Eq)]
 #[serde(rename_all = "camelCase")]
 pub struct Image {
-    novel_image_id: String,
-    sl: String,
-    urls: HashMap<String, String>,
+    pub novel_image_id: String,
+    pub sl: String,
+    pub urls: HashMap<String, String>,
 }
 
 #[derive(Deserialize, Serialize, Clone, Debug, PartialEq, Eq)]
 pub struct Rating {
-    bookmark: u64,
-    like: u64,
-    view: u64,
+    pub bookmark: u64,
+    pub like: u64,
+    pub view: u64,
 }
 #[derive(Deserialize, Serialize, Clone, Debug, PartialEq, Eq)]
 #[serde(rename_all = "camelCase")]
 pub struct SeriesNavigation {
-    next_novel: Option<NovelPrevNext>,
-    prev_novel: Option<NovelPrevNext>,
+    pub next_novel: Option<NovelPrevNext>,
+    pub prev_novel: Option<NovelPrevNext>,
 }
 
 #[derive(Deserialize, Serialize, Clone, Debug, PartialEq, Eq)]
 #[serde(rename_all = "camelCase")]
 pub struct NovelPrevNext {
-    content_order: String,
-    cover_url: String,
-    id: u64,
-    title: String,
-    viewable: bool,
-    viewable_message: Option<Value>,
+    pub content_order: String,
+    pub cover_url: String,
+    pub id: u64,
+    pub title: String,
+    pub viewable: bool,
+    pub viewable_message: Option<Value>,
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq)]
-struct NovelSeriesDetail1 {
-    caption: String,
-    content_count: i64,
-    display_text: String,
-    id: i64,
-    is_concluded: bool,
-    is_original: bool,
-    novel_ai_type: i64,
-    title: String,
-    total_character_count: i64,
-    user: User,
-    watchlist_added: bool,
+pub struct NovelSeriesDetail1 {
+    pub caption: String,
+    pub content_count: i64,
+    pub display_text: String,
+    pub id: i64,
+    pub is_concluded: bool,
+    pub is_original: bool,
+    pub novel_ai_type: i64,
+    pub title: String,
+    pub total_character_count: i64,
+    pub user: User,
+    pub watchlist_added: bool,
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq)]
 pub struct SeriesDetail {
-    next_url: Option<String>,
-    novel_series_detail: NovelSeriesDetail1,
-    novel_series_first_novel: Novel,
-    novel_series_latest_novel: Novel,
-    novels: Vec<Novel>,
+    pub next_url: Option<String>,
+    pub novel_series_detail: NovelSeriesDetail1,
+    pub novel_series_first_novel: Novel,
+    pub novel_series_latest_novel: Novel,
+    pub novels: Vec<Novel>,
 }
